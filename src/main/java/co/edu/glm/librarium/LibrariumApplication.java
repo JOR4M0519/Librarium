149c6e7bdd73126e4668918564733976f8aec9d5
David R <jpdramos1000@gmail.com>
2024-12-15 21:21:03 -0500

    init

---
+++ src/main/java/co/edu/glm/librarium/LibrariumApplication.java
@@ -0,0 +1,13 @@
+package co.edu.glm.librarium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrariumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrariumApplication.class, args);
+	}
+
+}
